1da24a802abc50aa11cf6f4f6441852b8c66fbc1
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-03-31 19:18:05 +0200

    fix(navigation): stub navigation actions

--- src/actions/navigation/index.ts
+++ src/actions/navigation/index.ts
@@ -5,75 +5,17 @@ import { AppError, ErrorCode } from 'src/lib/errors'
 import { withLoading, withErrorScreen } from 'src/actions/modifiers'
 import { ThunkAction } from 'src/store'
 
-const deferredNavActions: NavigationAction[] = []
-let dispatchNavigationAction = (action: NavigationAction) => {
-    deferredNavActions.push(action)
-  },
-  navigator: NavigationContainerComponent
-
-export const setTopLevelNavigator = (nav: NavigationContainerComponent) => {
-  dispatchNavigationAction = nav.dispatch.bind(nav)
-  navigator = nav
-  deferredNavActions.forEach(dispatchNavigationAction)
-  deferredNavActions.length = 0
-}
-
 /**
  * NOTE: navigate and navigatorReset both dispatch the navigation actions but
  * the actions are not handled by our reducers. Dispatching is useful for testing
  * (comparing snapshots of store actions) and it makes typescript happy
  */
-export const navigate = (
-  options: NavigationNavigateActionPayload,
-): ThunkAction => dispatch => {
-  const action = NavigationActions.navigate(options)
-
-  dispatchNavigationAction(action)
-  return dispatch(action)
+export const navigate = (options: any): ThunkAction => dispatch => {
+  return Promise.resolve()
 }
 
-export const navigatorReset = (
-  newScreen?: NavigationNavigateActionPayload,
-): ThunkAction => dispatch => {
-  const resetActionPayload: NavigationResetActionPayload = {
-    index: 0,
-    actions: [],
-  }
-
-  if (newScreen) {
-    resetActionPayload.actions.push(NavigationActions.navigate(newScreen))
-  } else {
-    // @ts-ignore
-    const navState = navigator.state.nav
-    // @ts-ignore
-    const navRouter: NavigationRouter = navigator._navigation.router
-
-    if (navRouter) {
-      const { path, params } = navRouter.getPathAndParamsForState(navState)
-      const action = navRouter.getActionForPathAndParams(
-        path,
-        params,
-      ) as NavigationNavigateAction
-
-      // getActionForPathAndParams is typed to potentially return null, but we are
-      // using it on the current state itself, so this should "never" happen
-      if (!action) {
-        throw new Error('impossible')
-      }
-
-      if (action.action) {
-        // since the top level router is a SwitchRouter,
-        // the first action will be to navigate to MainStack, but we are using a
-        // StackReset action which will be caught by MainStack, so we can just pass
-        // in the nested Navigate action
-        resetActionPayload.actions.push(action.action)
-      }
-    }
-  }
-
-  const resetAction = StackActions.reset(resetActionPayload)
-  dispatchNavigationAction(resetAction)
-  return dispatch(resetAction)
+export const navigatorReset = (newScreen?: any): ThunkAction => dispatch => {
+  return Promise.resolve()
 }
 
 export const navigatorResetHome = (): ThunkAction => dispatch =>
