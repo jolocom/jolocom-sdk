09df30f96569191e2bdf0be4a67b13ee07094c18
mnzaki <mnzaki@gmail.com>
2020-05-12 00:50:39 +0200

    refactor(storage/migrations): delete this.
    database migrations should be handled at application level

--- src/lib/storage/migration/index.ts
+++
@@ -1,9 +0,0 @@
-import { Initial1565886000404 } from './1565886000404-initial'
-import { ReencryptSeed1567674609659 } from './1567674609659-reencrypt-seed'
-import { Lib3To41580937907601 } from './1580937907601-Lib3To4'
-
-export const migrationList = [
-  Initial1565886000404,
-  ReencryptSeed1567674609659,
-  Lib3To41580937907601,
-]
