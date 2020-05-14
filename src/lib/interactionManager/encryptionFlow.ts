23b9ca2a8c06a9e7af3d1b548e24092cd77e35ac
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-05-14 13:06:10 +0200

    feat(enc flow): add encryption flow class

---
+++ src/lib/interactionManager/encryptionFlow.ts
@@ -0,0 +1,41 @@
+import { JWTEncodable } from 'jolocom-lib/js/interactionTokens/JSONWebToken'
+import { InteractionType } from 'jolocom-lib/js/interactionTokens/types'
+import { Interaction } from './interaction'
+import { Flow } from './flow'
+import { EncryptionFlowState } from './types'
+import { isRPCRequest, isRPCResult } from './guards'
+import { RPCRequest, RPCResponse, CallType } from './rpc'
+
+export class EncryptionFlow extends Flow {
+  public state: EncryptionFlowState = {
+    req: { rpc: CallType.AsymEncrypt, request: '' },
+  }
+
+  public constructor(ctx: Interaction) {
+    super(ctx)
+  }
+
+  public handleInteractionToken(
+    token: JWTEncodable,
+    interactionType: InteractionType,
+  ) {
+    switch (interactionType) {
+      case InteractionType.Authentication:
+        if (isRPCRequest(token)) return this.consumeEncryptionRequest(token)
+        else if (isRPCResponse(token))
+          return this.consumeEncryptionResponse(token)
+      default:
+        throw new Error('Interaction type not found')
+    }
+  }
+
+  public async consumeEncryptionRequest(token: RPCRequest) {
+    if (!this.state.req.request) this.state.req = token
+
+    return true
+  }
+
+  public async consumeEncryptionResult(token: RPCResult) {
+    return true
+  }
+}
