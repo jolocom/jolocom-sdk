4e23f9325b12720dd538edf6e057c8641f42e76d
mnzaki <mnzaki@gmail.com>
2020-06-16 18:07:30 +0200

    chore(packages): remove more react cruft

--- tests/utils/setup.ts
+++ tests/utils/setup.ts
@@ -1,15 +1,1 @@
 import 'regenerator-runtime'
-
-jest.mock('rn-fetch-blob', () => ({
-  __esModule: true,
-  default: {
-    DocumentDir: () => {},
-    polyfill: () => {},
-    fetch: jest.fn(),
-  },
-}))
-
-jest.mock('src/lib/storage/storage', () => ({
-  __esModule: true,
-  Storage: jest.fn(),
-}))
