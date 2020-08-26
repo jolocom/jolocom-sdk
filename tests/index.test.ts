7f6a5c2569859070fb16158d91e0fc1de13bb7ae
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-08-26 15:06:47 +0200

    test(enc): test encryption flow, fix sqlite

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -131,8 +131,72 @@ test('Resolution interaction', async () => {
   await service.processJWT(userResponse.encode())
 
   expect(
-    service.didMethods.getDefault().resolver.resolve(user.idw.did),
+    service.resolve(user.idw.did),
   ).resolves.toBeTruthy()
 
   await userCon.close()
 })
+
+test('Decryption Request interaction', async () => {
+  const serviceCon = await getConnection()
+  const userCon = await createConnection(testConnection2)
+
+  const serviceDB = new JolocomTypeormStorage(serviceCon)
+  const userDB = new JolocomTypeormStorage(userCon)
+
+  // in this test, the service is "anchored" (the user can always resolve them), and
+  const service = getSdk(serviceCon)
+  service.didMethods.setDefault(service.didMethods.get('jun'))
+  await service.init()
+
+  // insert the service's KEL to the user DB (make the service resolvable)
+  const serviceId = service.idw.did.split(':')[2]
+  const serviceEL = await serviceDB.eventDB.read(serviceId)
+
+  await userDB.eventDB.append(serviceId, serviceEL)
+
+  const user = getSdk(userCon)
+  // the user is "unanchored" (the service cannot resolve them initially)
+  user.didMethods.setDefault(user.didMethods.get('jun'))
+  await user.init()
+
+  // ensure the service is resolvable by the user
+  expect(
+    user.resolve(service.idw.did),
+  ).resolves.toBeTruthy()
+
+  // ensure the user is not resolvable by the service
+  expect(
+    service.resolve(user.idw.did),
+  ).rejects.toBeTruthy()
+
+  // create a resolution request
+  const serviceResRequest = await service.resolutionRequestToken()
+
+  // process the services request and get the handle for the interaction
+  const userResInteraction = await user.processJWT(serviceResRequest)
+
+  // create a resolution response
+  const userResponse = await userResInteraction.createResolutionResponse()
+
+  // process the resolution response, containing the state update proof of the User
+  await service.processJWT(userResponse.encode())
+
+  const data = Buffer.from("hello there")
+
+  const encryptedData = await service.idw.asymEncryptToDidKey(data, user.idw.publicKeyMetadata.encryptionKeyId, service.resolver)
+
+  const decReq = await service.rpcDecRequest({ toDecrypt: encryptedData, callbackURL: "nowhere" })
+
+  const userDecInteraction = await user.processJWT(decReq);
+
+  const decryptionResponse = await userDecInteraction.createDecResponseToken();
+
+  console.log(decryptionResponse.toJSON())
+
+  const serviceDecInteraction = await service.processJWT(decryptionResponse.encode())
+
+  console.log(serviceDecInteraction.getSummary().state)
+  
+  await userCon.close()
+}, 40000)
