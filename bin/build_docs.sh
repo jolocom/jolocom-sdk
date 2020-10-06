1df9d512701aeda282be5fd81a145bf4fd2db91b
mnzaki <mnzaki@gmail.com>
2020-10-06 16:24:59 +0200

    build(docs): add versioning

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -4,6 +4,7 @@ set -ex
 
 DOCS_BASE="docs"
 DOCS_BUILD_BASE="docs-build"
+DOCS_API_BASE="$DOCS_BASE/api"
 
 PKG_VERSION=`node -e 'console.log(require("./package.json").version)'`
 
@@ -11,10 +12,20 @@ DOCS_BUILD="${DOCS_BUILD_BASE}/${PKG_VERSION}"
 rm -rf "$DOCS_BUILD"
 
 mkdir -p "$DOCS_BUILD"
-typedoc --out "$DOCS_BASE/guides/api"
+typedoc --out "$DOCS_API_BASE"
+
+cat > "$DOCS_BUILD_BASE/index.html" <<EOF
+<html>
+<head>
+<title>Jolocom SDK</title>
+<meta http-equiv="refresh" content="0;URL='${PKG_VERSION}'" />
+</head>
+</html>
+EOF
 
 if [[ "$1" == "serve" ]]; then
-  mkdocs serve -f docs/mkdocs.yml
+  mkdocs serve -f mkdocs.yml
 else
-  mkdocs build -f docs/mkdocs.yml -d "`realpath "$DOCS_BUILD"`"
+  mkdocs build -f mkdocs.yml -d "`realpath "$DOCS_BUILD"`"
 fi
+
