5d79bdad117e15344cb75b3a4c920aa6dc241b08
Serhii Temchenko <serg.temchenko@gmail.com>
2021-09-27 14:32:11 +0200

    chore: naming correction

--- src/serviceContainer.ts
+++ src/serviceContainer.ts
@@ -1,16 +1,16 @@
 export class ServiceContainer {
-  private readonly plugins: Record<string, any> = {}
+  private readonly services: Record<string, any> = {}
 
   register(identifier: string, plugin: any, redefine = false) {
     const normalizedIdentifier = ServiceContainer.normalizeIdentifier(identifier)
 
-    if (this.plugins.hasOwnProperty(normalizedIdentifier) && !redefine) {
+    if (this.services.hasOwnProperty(normalizedIdentifier) && !redefine) {
       throw new Error(
         `Service registration failed. Plugin with name: '${normalizedIdentifier}' already registered.`,
       )
     }
 
-    this.plugins[normalizedIdentifier] = plugin
+    this.services[normalizedIdentifier] = plugin
 
     return this
   }
@@ -18,13 +18,13 @@ export class ServiceContainer {
   get<T>(identifier: string): T {
     const normalizedIdentifier = ServiceContainer.normalizeIdentifier(identifier)
 
-    if (!this.plugins.hasOwnProperty(normalizedIdentifier)) {
+    if (!this.services.hasOwnProperty(normalizedIdentifier)) {
       throw new Error(
         `Service with identifier: ${normalizedIdentifier} is not registered.`,
       )
     }
 
-    return this.plugins[normalizedIdentifier] as T
+    return this.services[normalizedIdentifier] as T
   }
 
   private static normalizeIdentifier(identifier: string): string {
